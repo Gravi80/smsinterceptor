43c3b45e29567645854c9d36ec2f4a6f101137e3
Ravi Sharma <ravis4376@gmail.com>
2015-08-01 17:58:51 +0530

    last commit

--- app/src/main/java/com/bootcamp/smsblocker/SmsReceiver.java
+++ app/src/main/java/com/bootcamp/smsblocker/SmsReceiver.java
@@ -5,11 +5,26 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.widget.Toast;
 
 public class SmsReceiver extends BroadcastReceiver{
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Bundle bundle= intent.getExtras();
+        if (bundle != null) {
+            Object[] smsObjects = (Object[])bundle.get("pdus");
+            SmsMessage[] messages = new SmsMessage[smsObjects.length];
+            StringBuilder sms = new StringBuilder();
+            for (int index = 0; index < smsObjects.length; index++) {
+                messages[index] = SmsMessage.createFromPdu((byte[]) smsObjects[index]);
+                sms.append(messages[index].getMessageBody());
+            }
+            String sender = messages[0].getOriginatingAddress();
+            String message = sender+" "+sms.toString();
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+            abortBroadcast();
+        }
     }
 
 
